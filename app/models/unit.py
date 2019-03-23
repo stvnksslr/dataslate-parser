ff8e3d74cecc55cad6b23b9d694ffdcff189d072
Steven kessler <stvnksslr@gmail.com>
2019-03-23 13:26:07 -0700

    progress on traits

--- app/models/unit.py
+++ app/models/unit.py
@@ -3,17 +3,17 @@ from dataclasses import dataclass
 
 @dataclass
 class Unit:
-    unit_name: str
-    movement: int
-    weapon_skill: int
-    ballistic_skill: int
-    strength: int
-    toughness: int
-    wounds: int
-    attacks: int
-    leadership: int
-    save: int
-    wargear: dict
-    abilities: dict
-    keywords: dict
-    point_cost: int
+    unit_name: str = ''
+    movement: int = 0
+    weapon_skill: int = 0
+    ballistic_skill: int = 0
+    strength: int = 0
+    toughness: int = 0
+    wounds: int = 0
+    attacks: int = 0
+    leadership: int = 0
+    save: int = 0
+    wargear: dict = ""
+    abilities: dict = ""
+    keywords: dict = ""
+    point_cost: int = 0
--- app/unit_parser.py
+++ app/unit_parser.py
@@ -1,11 +1,19 @@
 from bs4 import BeautifulSoup
-from app.models.kt_models import kt_unit
+
+from app.models.unit import Unit
+
 
 class UnitParser:
     @staticmethod
     def parse_units(contents):
-        soup = BeautifulSoup(contents, "html")
+        soup = BeautifulSoup(contents, "lxml")
         unit_list = soup.findAll("selection", {"type": "model"})
-        for unit in unit_list:
-            unit_name = unit.attrs.get('name')
+        parsed_unit_list = []
+
+        for item in unit_list:
+            parsed_unit_name = item.attrs.get('name')
+            parsed_movement = item.contents[3].contents[1].contents[9].contents[1].attrs.get('value')
+            parsed_unit = Unit(unit_name=parsed_unit_name,
+                               movement=parsed_movement)
+            parsed_unit_list.append(parsed_unit)
         return unit_list
