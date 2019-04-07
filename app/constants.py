c7b0da6f215d09e715fc378a8b842cbaacd3d38d
Steven kessler <stvnksslr@gmail.com>
2019-04-07 18:22:27 -0400

    refactor(abstracting unit class, new constants):

--- app/constants.py
+++ app/constants.py
@@ -1,3 +1,8 @@
-KILLTEAM_ID = "a467-5f42-d24c-6e5b"
-KILLTEAM_NAME = "Warhammer 40,000: Kill Team (2018)"
+KILLTEAM_ID: str = "a467-5f42-d24c-6e5b"
+KILLTEAM_NAME: str = "Warhammer 40,000: Kill Team (2018)"
 
+HORUS_HERESY_ID: str = ""
+HORUS_HERESY_NAME: str = ""
+
+WARHAMMER_40K_ID: str = ""
+WARHAMMER_40K_NAME: str = ""
--- app/models/kt_unit.py
+++ app/models/kt_unit.py
@@ -1,20 +1,10 @@
 from dataclasses import dataclass
 
+from app.models.unit import Unit
+
 
 @dataclass
-class KtUnit:
-    unit_name: str = ""
-    wargear: dict = ""
-    abilities: dict = ""
+class KtUnit(Unit):
+    max: str = ""
     keywords: list = ""
     point_cost: int = 0
-    movement: str = ""
-    weapon_skill: str = ""
-    ballistic_skill: str = ""
-    strength: str = ""
-    toughness: str = ""
-    wounds: str = ""
-    attacks: str = ""
-    leadership: str = ""
-    save: str = ""
-    max: str = ""
