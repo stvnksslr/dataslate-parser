093c0339de31241d49934f6c5df8f3dc45cf9c5e
Steven kessler <stvnksslr@gmail.com>
2019-09-11 13:13:35 -0400

    feat(tests for rest endpoints):

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from fastapi import FastAPI
-from fastapi import FastAPI
 from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 
-@app.get("/items/{id}")
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+@app.get("/")
+async def root():
+    return {"msg": "Hello World"}
+
+
+@app.get("/render/killteam")
+async def read_item(request: Request):
+    return templates.TemplateResponse("item.html", {"request": request})
+
+
+@app.get("/render/30k")
+async def read_item(request: Request):
+    return templates.TemplateResponse("item.html", {"request": request})
+
+
+@app.get("/render/40k")
+async def read_item(request: Request):
+    return templates.TemplateResponse("item.html", {"request": request})
