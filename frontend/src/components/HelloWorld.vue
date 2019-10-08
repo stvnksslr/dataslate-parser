93445d4b81becba8772730289e26fd4aca5b3455
Steven Kessler <stvnksslr@gmail.com>
2019-10-07 22:59:26 -0700

    chore(sorting actions):

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p> You're one stop shop for creating printer friendly outputs for battlescribe lists </p>
+    <p>
+      You're one stop shop for creating printer friendly outputs for
+      battlescribe lists
+    </p>
   </div>
 </template>
 
--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <p>This project is inspired by and helped out by some awesome OSS applications already out there within the community mainly</p>
+    <p>
+      This project is inspired by and helped out by some awesome OSS
+      applications already out there within the community mainly
+    </p>
     <ul>
       <a href="https://github.com/gregchiasson/warhams">buttsribe</a>
     </ul>
     <ul>
-      <a href="https://codepen.io/esdictor/pen/bRamPy">8th Edition Unit CSS unit cards</a>
+      <a href="https://codepen.io/esdictor/pen/bRamPy"
+        >8th Edition Unit CSS unit cards</a
+      >
     </ul>
   </div>
 </template>
