1f51ef855ae9fd01778486be7652ee24a817bcc9
stvnksslr <stvnksslr@gmail.com>
2019-10-14 19:56:57 -0700

    refactor(clean up): now that the app is working e2e, with deploy working on cleanup

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 from app.utils.test_utils import (
-    generate_test_roster_heresy,
-    get_parser_and_parse_roster,
+    get_parser_type_and_parse,
 )
 
 app = FastAPI(
@@ -24,46 +23,12 @@ async def root(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
 
-@app.get("/render/kt/example")
-def example(request: Request):
-    return templates.TemplateResponse("40k_example.html", {"request": request})
-
-
-@app.get("/render/heresy/example")
-def heresy_example(request: Request):
-    test_roster = generate_test_roster_heresy()
-
-    return templates.TemplateResponse(
-        "sandbox.html", {"request": request, "roster": test_roster}
-    )
-
-
 @app.post("/files/")
 async def create_file(request: Request, file: bytes = File(...)):
-    data = get_parser_and_parse_roster(roster=file)
+    data = get_parser_type_and_parse(roster=file)
     parsed_roster = data.get("roster")
     template = data.get("template")
 
     return templates.TemplateResponse(
         template, {"request": request, "roster": parsed_roster}
     )
-
-
-@app.get("/render/kt")
-async def render_kt(request: Request):
-    return templates.TemplateResponse("item.html", {"request": request})
-
-
-@app.get("/render/kt/roster")
-async def render_kt_roster(request: Request):
-    return templates.TemplateResponse("item.html", {"request": request})
-
-
-@app.get("/render/30k")
-async def render_heresy(request: Request):
-    return templates.TemplateResponse("item.html", {"request": request})
-
-
-@app.get("/render/40k")
-async def render_40k(request: Request):
-    return templates.TemplateResponse("item.html", {"request": request})
--- app/utils/test_utils.py
+++ app/utils/test_utils.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 from app.parsers.gametype.gametype import (
     detect_gametype,
     find_gametype_parser,
@@ -16,30 +14,10 @@ def fetch_and_parse_roster(roster_file):
         return parsed_roster
 
 
-def get_parser_and_parse_roster(roster):
+def get_parser_type_and_parse(roster):
     gametype = detect_gametype(roster)
     parser = find_gametype_parser(gametype)
     template = find_template(gametype)
 
     parsed_roster = parser.parse_units(contents=roster)
     return {"roster": parsed_roster, "template": template}
-
-
-def generate_test_roster_heresy():
-    base_path = Path.cwd() / "test_rosters" / "horus_heresy"
-    chaos_kill_team_standard = str(base_path / "parser_test_full_list.ros")
-    gametype = "heresy"
-    parsed_roster = fetch_and_parse_roster(
-        roster_file=chaos_kill_team_standard, gametype=gametype
-    )
-    return parsed_roster
-
-
-def generate_test_roster():
-    base_path = Path.cwd() / "test_rosters" / "kill_team"
-    chaos_kill_team_standard = str(base_path / "test_roster_commander.ros")
-    gametype = "killteam"
-    parsed_roster = fetch_and_parse_roster(
-        roster_file=chaos_kill_team_standard, gametype=gametype
-    )
-    return parsed_roster
