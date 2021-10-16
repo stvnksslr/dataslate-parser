ab51e32bcf2c689ca91fd07a94c5f79c0b5aa410
Jérôme MARCHAND <jerome.marchand@zenika.com>
2021-10-17 00:18:37 +0200

    Add icon in W40K datasheets

--- src/main_test.py
+++ src/main_test.py
@@ -20,11 +20,14 @@ def test__read_main():
 def test__upload_roster():
     multiple_pages = False
     summary_page = False
+    use_icons = False
     with open(test_roster, "r") as roster_file:
         roster_file = roster_file.read()
 
     response = client.post(
-        "/files/", files={"file": roster_file}, data={"multiple_pages": multiple_pages, "summary_page": summary_page}
+        "/files/",
+        files={"file": roster_file},
+        data={"multiple_pages": multiple_pages, "summary_page": summary_page, "use_icons": use_icons}
     )
 
     assert response.status_code == 200
@@ -33,11 +36,14 @@ def test__upload_roster():
 def test__upload_roster_multipart_false():
     multiple_pages = False
     summary_page = False
+    use_icons = False
     with open(test_roster, "r") as roster_file:
         roster_file = roster_file.read()
 
     response = client.post(
-        "/files/", files={"file": roster_file}, data={"multiple_pages": multiple_pages, "summary_page": summary_page}
+        "/files/",
+        files={"file": roster_file},
+        data={"multiple_pages": multiple_pages, "summary_page": summary_page, "use_icons": use_icons}
     )
     multiple_pages_response = "single_page.css" in response.text
 
@@ -48,12 +54,15 @@ def test__upload_roster_multipart_false():
 def test__upload_roster_multipart_true():
     multiple_pages = True
     summary_page = False
+    use_icons = False
 
     with open(test_roster, "r") as roster_file:
         roster_file = roster_file.read()
 
     response = client.post(
-        "/files/", files={"file": roster_file}, data={"multiple_pages": multiple_pages, "summary_page": summary_page}
+        "/files/",
+        files={"file": roster_file},
+        data={"multiple_pages": multiple_pages, "summary_page": summary_page, "use_icons": use_icons}
     )
 
     multiple_pages_response = "multiple_pages.css" in response.text
